9bf1391c91aff99b22e3372fa79fedadfccf2f24
Leno <akrachunov@gmail.com>
2021-03-08 21:43:30 +0000

    Improve navigation styling.

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,9 +3,9 @@ import logo from '../logo.png'
 export default function Navigation() {
     return (
         <nav className="navbar custom-nav">
-            <div className="container">
+            <div className="container pl-6 pr-6">
                 <div className="navbar-brand">
-                    <img src={logo} alt='logo' className="logo-styles"/>
+                    <img src={logo} alt='logo' className="logo-styles mr-3"/>
                     <a className="navbar-burger" role="button" aria-label="menu" aria-expanded="false">
                         <span aria-hidden="true"></span>
                         <span aria-hidden="true"></span>
@@ -14,19 +14,15 @@ export default function Navigation() {
                 </div>
                 <div className="navbar-menu">
                     <div className="navbar-start">
-                        <a className="navbar-item" href="#">Features</a><a className="navbar-item" href="#">Enterprise</a><a
-                        className="navbar-item" href="#">Support</a>
-                        <div className="navbar-item has-dropdown is-hoverable">
-                            <a className="navbar-link">ICO</a>
-                            <div className="navbar-dropdown"><a className="navbar-item navbar-item-dropdown"
-                                                                href="#">Whitepaper</a><a
-                                className="navbar-item navbar-item-dropdown" href="#">Token</a></div>
-                        </div>
+                        <a className="navbar-item has-text-weight-semibold" href="#">Writers</a>
+                        <a className="navbar-item has-text-weight-semibold" href="#">About Us</a>
                     </div>
                     <div className="navbar-end">
+                        <input className="input is-align-self-center is-size-7 mr-6 mt-2" type="text" placeholder="Find writers or stories"/>
                         <div className="navbar-item">
-                            <div className="buttons"><a className="button is-light" href="#">Log in</a><a
-                                className="button is-primary" href="#">Sign up</a></div>
+                            <div className="buttons">
+                                <a className="has-text-weight-semibold sign-style" href="#">Sign in</a>
+                            </div>
                         </div>
                     </div>
                 </div>
