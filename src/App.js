31bd040f35f09de786ec9f27a4aa33d6a82969e8
Leno <akrachunov@gmail.com>
2021-03-21 16:36:48 +0000

    Add basic custom styles to the Bulma timeline.

--- src/App.js
+++ src/App.js
@@ -13,11 +13,15 @@ import useGlobalState from "./globalState";
 import RouteAuthenticated from "./protectedRoute";
 import ProfilePage from "./pages/ProfilePage";
 import Loading from "./components/Loading";
+import Article from "./components/Article";
 
 function App() {
     const g =   useGlobalState();
+    const db = firebase.firestore();
+    const categoriesRef = db.collection("categories");
 
     React.useEffect(() => {
+        // Figure out authentication
         setTimeout(() => {
             firebase.auth().onAuthStateChanged(function(user) {
                 if (user) {
@@ -27,11 +31,27 @@ function App() {
                 }
             });
         }, 2000)
+
+        // Get available categories
+        categoriesRef.where("articles", ">", 0).get()
+            .then((querySnapshot) => {
+                let categories = [];
+                querySnapshot.forEach((doc) => {
+                    // doc.data() is never undefined for query doc snapshots
+                    categories.push(doc.data().category);
+                });
+                g.setArticle({type: "categories", payload: categories});
+            })
+            .catch((error) => {
+                console.log("Error getting documents: ", error);
+            });
+
     }, []);
     return (
         <div className="page-wrapper">
             {g.s.manage.isAuthenticated === null ? <Loading /> :
                 <Fragment>
+                    {g.s.article.modalIsOpen && <Article />}
                     <Navigation />
                         <div className="content-wrapper">
                             <Switch>
@@ -48,6 +68,6 @@ function App() {
             }
         </div>
     );
-}
+};
 
 export default App;
