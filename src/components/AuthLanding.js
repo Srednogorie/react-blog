15e05f0c7cbf0f8528b4b62ad23919b20120105a
Leno <akrachunov@gmail.com>
2021-03-21 16:35:44 +0000

    Add authenticated user home page.

---
+++ src/components/AuthLanding.js
@@ -0,0 +1,36 @@
+function AuthLanding() {
+    return (
+        <div className="timeline is-centered">
+            <div className="timeline-item is-primary">
+                <div className="timeline-marker is-primary is-image is-64x64">
+                    <img src="https://firebasestorage.googleapis.com/v0/b/mysoftexam.appspot.com/o/article_images%2Fculture_one.jpeg?alt=media&token=2f2c1345-52ca-4509-a303-0cb729564e2c"/>
+                </div>
+                <div className="timeline-content">
+                    <p className="heading">January 2016</p>
+                    <p>Timeline content - Can include any HTML element</p>
+                </div>
+            </div>
+            <div className="timeline-item is-primary">
+                <div className="timeline-marker is-primary is-image is-64x64">
+                    <img className="timeline-image" src="https://firebasestorage.googleapis.com/v0/b/mysoftexam.appspot.com/o/article_images%2Fmusic_one.jpeg?alt=media&token=f920cc6b-b1e6-42cb-9628-a349205e4b68"/>
+                </div>
+                <div className="timeline-content">
+                    <p className="heading">January 2016</p>
+                    <p>Timeline content - Can include any HTML element</p>
+                </div>
+            </div>
+            <div className="timeline-item is-primary">
+                <div className="timeline-marker is-primary is-image is-64x64">
+                    <img src="https://firebasestorage.googleapis.com/v0/b/mysoftexam.appspot.com/o/article_images%2Fculture_one.jpeg?alt=media&token=2f2c1345-52ca-4509-a303-0cb729564e2c"/>
+                </div>
+                <div className="timeline-content">
+                    <p className="heading">January 2016</p>
+                    <p>Timeline content - Can include any HTML element</p>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default AuthLanding;
