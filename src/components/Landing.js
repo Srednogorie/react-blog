87d7a1fdea79b0eff383562f1572ed2aded4e9cd
Leno <akrachunov@gmail.com>
2021-03-21 16:48:09 +0000

    Move articles fetch to the parent HomePage component.

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,44 +1,70 @@
+import useGlobalState from "../globalState";
+import {Link} from "react-router-dom";
+import React from "react";
+import firebase from "../firebase";
+import {truncate} from "../utils";
+import AuthLanding from "./AuthLanding";
+
 function Landing() {
+    const g = useGlobalState();
+    const showArticle = (event) => {
+        g.setArticle({type: "modal_is_open", payload: true});
+    }
+    const handleCategoryChange = (event, index) => {
+        g.setArticle({type: "active_category", payload: index});
+        const currentArticles = g.s.article.nonAuthArticles;
+        const currentArticle = currentArticles.filter(obj => obj.category === g.s.article.categories[index])[0];
+        g.setArticle({type: "non_auth_articles_current", payload: currentArticle});
+    }
+
     return (
         <div className="container px-6 landing-main">
+            {!g.s.manage.isAuthenticated ?
             <div className="landing-pub">
                 <div>
                     <h2 className="landing-pub-header">Latest Publication</h2>
                 </div>
                 <div className="landing-pub-pub">
-                    <a className="landing-publication" href="#">
-                        <div className="landing-publication-content">
-                            <div className="landing-pub-icon">
-                                <a href="https://drawtogether.substack.com" >
-                                <img src="https://cdn.substack.com/image/fetch/w_196,h_196,c_fill,f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fbucketeer-e05bbc84-baa3-437e-9518-adb32be77984.s3.amazonaws.com%2Fpublic%2Fimages%2F37fe3a40-ce47-4c7c-a043-71a41b9003c7_256x256.png"
-                                width="196" className="landing-custom" alt=""/>
-                                </a>
-                            </div>
-                            <div className="landing-publication-text-area">
-                                <div className="landing-publication-title">DrawTogether with WendyMac</div>
-                                <div className="landing-publication-author">Launched a month ago</div>
-                                <div className="landing-publication-description">The show that’s a class that’s a club - for
-                                    kids! New shows, activities, projects, and special guests delivered to your inbox
-                                    each week.
+                    {g.s.article.nonAuthArticlesCurrent &&
+                        <Link to="#" onClick={showArticle} className="landing-publication">
+                            <div className="landing-publication-content">
+                                <div className="landing-pub-icon">
+                                    <img src={g.s.article.nonAuthArticlesCurrent.image_url} className="landing-custom" alt=""/>
+                                </div>
+                                <div className="landing-publication-text-area">
+                                    <div
+                                        className="landing-publication-title">{g.s.article.nonAuthArticlesCurrent.title}</div>
+                                    <div
+                                        className="landing-publication-author">{g.s.article.nonAuthArticlesCurrent.subtitle}</div>
+                                    <div
+                                        className="landing-publication-description">{truncate(g.s.article.nonAuthArticlesCurrent.content, 150)}</div>
                                 </div>
                             </div>
-                        </div>
-                    </a>
+                        </Link>
+                    }
                 </div>
             </div>
+            : <AuthLanding />
+            }
             <div className="landing-categories">
                 <h2 className="landing-cat-title">Categories</h2>
                 <div className="landing-cat-cat">
-                    <button className="cat-button active" type="button">Music</button>
-                    <button className="cat-button" type="button">Culture</button>
-                    <button className="cat-button" type="button">Politics</button>
-                    <button className="cat-button" type="button">Technology</button>
-                    <button className="cat-button" type="button">Business</button>
-                    <button className="cat-button" type="button">Finance</button>
+                    {
+                        g.s.article.categories.length > 0 &&
+                        g.s.article.categories.map((category, index) => {
+                            return (
+                                <button
+                                    onClick={(event) => {handleCategoryChange(event, index)}}
+                                    key={index} className={`cat-button ${g.s.article.activeCategory === index && "active"}`}
+                                    type="button">{category}
+                                </button>
+                            )
+                        })
+                    }
                 </div>
             </div>
         </div>
     )
-}
+};
 
 export default Landing;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,13 +1,45 @@
 import Header from "../components/Header";
 import Landing from "../components/Landing";
 import useGlobalState from "../globalState";
+import React from 'react';
+import firebase from "../firebase";
 
 function HomePage() {
-    const g =   useGlobalState();
-    console.log(g.s.manage.isAuthenticated);
+    const g = useGlobalState();
+    const db = firebase.firestore();
+    const articlesRef = db.collection("articles");
+    React.useEffect(() => {
+        const categories = g.s.article.categories;
+        let unauthenticatedArticles = [];
+        categories.forEach((category) => {
+            // Get articles for unauthenticated users based on available categories
+            articlesRef.where("category", "==", category).orderBy("created", "desc").limit(1).get()
+                .then((querySnapshot) => {
+                    querySnapshot.forEach((doc) => {
+                        let docData = doc.data();
+                        docData['key'] = doc.id;
+                        unauthenticatedArticles.push(docData);
+                    });
+                    g.setArticle({type: "non_auth_articles", payload: unauthenticatedArticles});
+                })
+                .catch((error) => {
+                    console.log("Error getting documents: ", error);
+                });
+        })
+    }, [])
+    React.useEffect(() => {
+        // Set the current article
+        const currentCategory = g.s.article.categories[0];
+        const currentArticles = g.s.article.nonAuthArticles;
+        const currentArticle = currentArticles.filter(obj => obj.category === currentCategory)[0];
+        g.setArticle({type: "non_auth_articles_current", payload: currentArticle});
+        return () => {
+            g.setArticle({type: "active_category", payload: 0});
+        }
+    }, [g.s.article.nonAuthArticles])
     return (
         <div>
-            <Header />
+            {!g.s.manage.isAuthenticated && <Header />}
             <Landing />
         </div>
     )
