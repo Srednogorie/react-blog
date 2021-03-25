caf1c754118ee35f5761ab45702230d99014e304
Leno <akrachunov@gmail.com>
2021-03-25 21:41:56 +0000

    Remove stuff.

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import { Route, Switch } from "react-router-dom";
 import Navigation from "./components/Navigation";
 import Footer from "./components/Footer";
 import HomePage from "./pages/HomePage";
-import WritersPage from "./pages/WritersPage";
-import AboutPage from "./pages/AboutPage";
 import SignPage from "./pages/SignPage";
 import Register from "./pages/Register";
 import firebase from "firebase";
@@ -13,7 +11,6 @@ import useGlobalState from "./globalState";
 import RouteAuthenticated from "./protectedRoute";
 import ProfilePage from "./pages/ProfilePage";
 import Loading from "./components/Loading";
-import Article from "./components/Modal";
 import Modal from "./components/Modal";
 
 function App() {
@@ -57,9 +54,7 @@ function App() {
                         <div className="content-wrapper">
                             <Switch>
                                 <Route path="/" exact component={HomePage} />
-                                <RouteAuthenticated path="/writers" component={WritersPage} />}
                                 <RouteAuthenticated path="/profile" component={ProfilePage} />
-                                <Route path="/about" exact component={AboutPage} />
                                 <Route path="/sign" exact component={SignPage} />
                                 <Route path="/register" exact component={Register} />
                             </Switch>
